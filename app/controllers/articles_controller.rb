15860d4d4e305474ee69949d86edc7cc5a778523
Steve-Drewery <11940@coderacademy.edu.au>
2022-06-16 12:21:01 +1000

    params for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,27 @@ class ArticlesController < ApplicationController
         @time = Time.now
         @articles = Article.all
     end
+
+    def show
+        @article = Article.find(params[:id])
+    end
+
+    def new
+        @article = Article.new
+    end
+
+    def create
+        @article = Article.new(article_params)
+
+        if @article.save
+        redirect_to articles_path
+        else
+            render :new
+        end
+    end
+
+    private
+    def article_params
+        params.require(:article).permit(:title)
+    end
 end
