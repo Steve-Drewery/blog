1ef18dfcd5257d95edce97c99b222a11c02f0106
Steve_Drewery <11940@coderacademy.edu.au>
2022-06-23 22:27:25 +1000

    before action added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+
+    before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
     def index
         @time = Time.now
         @articles = Article.all
