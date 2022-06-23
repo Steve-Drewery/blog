fc803918da3dbd5deb886f6f6d21db1da2a95f54
Steve-Drewery <11940@coderacademy.edu.au>
2022-06-23 16:06:56 +1000

    comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'articles#index', as: "home"
   # get 'articles', to: 'articles#index'
   # get 'articles/new', to: 'articles#new'
-  resources :articles
+  resources :articles do 
+    resources :comments
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
