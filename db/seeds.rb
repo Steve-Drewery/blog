bd1784d673f5fc8247791b2bebe903f370752cad
Steve-Drewery <11940@coderacademy.edu.au>
2022-06-23 19:56:59 +1000

    faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do |i|
+    Article.create([
+    { 
+        title: Faker::TvShows::DrWho.quote, 
+        body: Faker::Quotes::Shakespeare.as_you_like_it_quote, 
+        importance: rand(1..10)}
+    ])
+end
